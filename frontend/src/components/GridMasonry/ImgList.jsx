33227f53909875dfb96f3f89d7d1435039488296
Alejo9601 <alejandro96jm@gmail.com>
2026-02-09 17:28:04 -0300

    UPDATE | Added new validation on map sentence

--- frontend/src/components/GridMasonry/ImgList.jsx
+++ frontend/src/components/GridMasonry/ImgList.jsx
@@ -1,10 +1,11 @@
+import { useEffect } from "react";
 import useUser from "../../hooks/useUser";
 import ImgItem from "./ImgItem";
 
 const ImgList = ({ imgsArray, onDeleteBtnClick }) => {
    return (
       <>
-         {imgsArray?.map((image) => {
+         {(imgsArray || []).map((image) => {
             return (
                <ImgItem
                   key={image.id}
