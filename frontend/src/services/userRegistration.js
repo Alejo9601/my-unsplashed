d8dac6857a79fd082d065c4bbe7e39a9d5b9e4be
Alejo9601 <alejandro96jm@gmail.com>
2026-02-26 16:27:48 -0300

    UPDATE | Changed cache parameter

--- frontend/src/services/userRegistration.js
+++ frontend/src/services/userRegistration.js
@@ -4,6 +4,7 @@ export async function userRegistration(username, password) {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ username: username, password: password }),
+      cache: "no-store",
       credentials: "include",
    };
 
